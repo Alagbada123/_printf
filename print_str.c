28c4130e8d7e337e1d0581e737ef309257af9631
Cutiesamba01 <rhodagbemi@gmail.com>
2024-07-29 19:30:23 +0100

    corrected task 1

--- print_str.c
+++ print_str.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * printf_string - print a string.
  * @val: argument.
- * Return: the value of the string
+ * Return: the length of the string
  */
 
 int printf_string(va_list val)
@@ -14,16 +15,13 @@ int printf_string(va_list val)
 	if (str == NULL)
 	{
 		str = "(null)";
-		len = _strlen(str);
-		for (i = 0; i < len; i++)
-			_putchar(str[i]);
-		return (len);
 	}
-	else
+
+	len = _strlen(str);
+	for (i = 0; i < len; i++)
 	{
-		len = _strlen(str);
-		for (i = 0; i < len; i++)
-			_putchar(str[i]);
-		return (len);
+		_putchar(str[i]);
 	}
+
+	return (len);
 }
