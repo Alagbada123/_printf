16015592b1632aa08c8b27b87a9d6b1ba4c10fbc
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-07-29 02:21:36 -0700

    This is edited

--- print_Rot13.c
+++ print_Rot13.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * print_Rot13 - converts to Rot13
- * @val: arguments
+ * @args: printf arguments
  * Return: count
  *
  */
 
-int print_Rot13(va_list val)
+int print_Rot13(va_list args)
 {
 	int a, b, count = 0;
 	int c = 0;
-	char *s = va_arg(val, char*);
+	char *s = va_arg(args, char*);
 	char u[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 	char v[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
